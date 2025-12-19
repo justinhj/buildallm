05a075f2ee8525c9f453597580d1f1c12a91958f
Justin Heyes-Jones <justinhj@gmail.com>
2025-12-18 18:16:37 -0800

    basic tokenizer

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
 import re
 
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+
 def main():
     with open("./data/the-verdict.txt", "r", encoding="utf-8") as f:
         raw_text = f.read()
     print("Total number of characters:", len(raw_text))
 
-    sample = raw_text[100:199]
+    text = raw_text
 
-    print(sample)
+    # print(text)
 
-    result = re.split(r'([,.]|\s)', sample)
+    result = re.split(r'([,.:;?_!"()\']|--|\s)', text)
     result = [item for item in result if item.strip()]
-    print(result)
+    print(result[:30])
+    print(len(result))
+
+    preprocessed = result
+
+    all_words = sorted(set(preprocessed))
+    vocab_size = len(all_words)
+    print(vocab_size)
+
+    vocab = {token: integer for integer, token in enumerate(all_words)}
+    for i, item in enumerate(vocab.items()):
+        print(item)
+        if i >= 50:
+            break
+
+    tokenizer = SimpleTokenizerV1(vocab)
+    text = """"It's the last he painted, you know,"
+           Mrs. Gisburn said with pardonable pride."""
+    ids = tokenizer.encode(text)
+    print(ids)
+
+    print(tokenizer.decode(ids))
 
 
 if __name__ == "__main__":
