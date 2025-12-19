915e903ffc7fd8f0c6eb82de4bf3d7936c70c5f6
Justin Heyes-Jones <justinhj@gmail.com>
2025-12-18 21:03:26 -0800

    tokenizer 2

--- main.py
+++ main.py
@@ -21,6 +21,29 @@ class SimpleTokenizerV1:
         return text
 
 
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        preprocessed = [item if item in self.str_to_int
+                        else "<|unk|>" for item in preprocessed]
+
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
+        return text
+
+
 def main():
     with open("./data/the-verdict.txt", "r", encoding="utf-8") as f:
         raw_text = f.read()
@@ -37,18 +60,16 @@ def main():
 
     preprocessed = result
 
-    all_words = sorted(set(preprocessed))
-    vocab_size = len(all_words)
-    print(vocab_size)
+    all_tokens = sorted(list(set(preprocessed)))
+    all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+    vocab = {token: integer for integer, token in enumerate(all_tokens)}
 
-    vocab = {token: integer for integer, token in enumerate(all_words)}
-    for i, item in enumerate(vocab.items()):
+    print(len(vocab.items()))
+    for i, item in enumerate(list(vocab.items())[-5:]):
         print(item)
-        if i >= 50:
-            break
 
-    tokenizer = SimpleTokenizerV1(vocab)
-    text = """"It's the last he painted, you know,"
+    tokenizer = SimpleTokenizerV2(vocab)
+    text = """"It's the last he heyes painted, you know,"
            Mrs. Gisburn said with pardonable pride."""
     ids = tokenizer.encode(text)
     print(ids)
