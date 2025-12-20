4dced782aaa0dd233152ff366758fb87cf828bf5
Justin Heyes-Jones <justinhj@gmail.com>
2025-12-19 19:59:57 -0800

    torch play

--- main.py
+++ main.py
@@ -1,6 +1,45 @@
 import re
 from importlib.metadata import version
 import tiktoken
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class GPTDatasetV1(Dataset):
+    def __init__(self, txt, tokenizer, max_length, stride):
+        self.input_ids = []
+        self.target_ids = []
+
+        # Convert the incoming text to tokens
+        token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i:i + max_length]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+
+    def __len__(self):
+        return len(self.input_ids)
+
+    def __getitem__(self, idx):
+        return self.input_ids[idx], self.target_ids[idx]
+
+
+def create_dataloader_v1(txt, batch_size=4, max_length=256,
+                         stride=128, shuffle=True, drop_last=True,
+                         num_workers=0):
+    tokenizer = tiktoken.get_encoding("gpt2")
+    dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        drop_last=drop_last,
+        num_workers=num_workers
+    )
+
+    return dataloader
 
 
 class SimpleTokenizerV1:
@@ -47,24 +86,29 @@ class SimpleTokenizerV2:
 
 
 def main():
+    print(f'Torch loaded. version {torch.__version__}')
+    print(f'MPS available: {torch.backends.mps.is_available()}')
     print("tiktoken version:", version("tiktoken"))
 
     with open("./data/the-verdict.txt", "r", encoding="utf-8") as f:
         raw_text = f.read()
 
-    print("Total number of characters:", len(raw_text))
+    dataloader = create_dataloader_v1(
+        raw_text, batch_size=1, max_length=4, stride=1, shuffle=False)
+    data_iter = iter(dataloader)
+    first_batch = next(data_iter)
+    print(first_batch)
+    second_batch = next(data_iter)
+    print(second_batch)
 
-    tokenizer = tiktoken.get_encoding("gpt2")
+    vocab_size = 6
+    output_dim = 3
 
-    enc_text = tokenizer.encode(raw_text)
-    print(len(enc_text))
+    torch.manual_seed(123)
+    embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+    print(embedding_layer.weight)
+    print(embedding_layer(torch.tensor([3])))
 
-    enc_sample = enc_text[50:]
-    context_size = 4
-    x = enc_sample[:context_size]
-    y = enc_sample[1:context_size+1]
-    print(f"x: {x}")
-    print(f"y:      {y}")
 
 if __name__ == "__main__":
     main()
